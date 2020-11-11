8c9f43ff4007c8a750dfebf9ef968ce0d532ae8c
Sean <skabe89@gmail.com>
2020-11-11 14:34:49 -0500

    commit tree 45a852aba199e3231c312c6628e2c933271546fd

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
 def my_all?(collection)
-
+    i = 0
+    place = []
+    while i < collection.length
+        place << yield(collection[i])
+        i += 1
+    end
+#binding.pry
+    if place.include?(false)
+        false
+    else
+        true
+    end
 end
